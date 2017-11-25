4e0a02258b9232a891cbb74d4af78f6f1990db79
Chino-sama <beto4600@hotmail.com>
2017-11-24 18:46:16 -0600

    feat: create athors, editorials and genres

--- app/controllers/admin/books/new.js
+++ app/controllers/admin/books/new.js
@@ -1,16 +1,53 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	// genreOptions: {
-	// 	autocompleteOptions: {
-	// 		data: {
-	// 			'Apple': null,
-	// 			'Microsoft': null,
-	// 			'Google': null
-	// 		},
-	// 		limit: Infinity,
-	// 		minLength: 1
-	//     }
-	// }
+	actions: {
+		saveAuthor(name, lastName) {
+			this.store.createRecord('author', {
+				name,
+				lastName
+			}).save().then(()=>{
+				this.set('name', null);
+				this.set('lastName', null);
+				swal(
+					'Autor Registrado',
+					'',
+					'success'
+				).then(()=>{
+					Ember.$('#editorial-modal').modal('close');
+				});
+			})
+		},
+		saveEditorial(name, image) {
+			this.store.createRecord('editorial', {
+				name,
+				image
+			}).save().then(()=>{
+				this.set('editorialName', null);
+				this.set('editorialImg', null);
+				swal(
+					'Editorial Registrada',
+					'',
+					'success'
+				).then(()=>{
+					Ember.$('#editorial-modal').modal('close');
+				});
+			});
+		},
+		saveGenre(name) {
+			this.store.createRecord('genre', {
+				name
+			}).save().then(()=>{
+				this.set('genreName', null);
+				swal(
+					'Género Registrado',
+					'',
+					'success'
+				).then(()=>{
+					Ember.$('#genre-modal').modal('close');
+				});
+			});
+		}
+	}
 });
 	
--- app/models/editorial.js
+++ app/models/editorial.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
 	name: 	DS.attr('string'),
-	logo: 	DS.attr('string'),
+	image: 	DS.attr('string'),
 
 	books: 	DS.hasMany('books')
 });
